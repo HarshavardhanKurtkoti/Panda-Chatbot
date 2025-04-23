33127a6d5147a2473918801856957b8105615a77
Harshavardhan <kurtkoti.harsha@gmail.com>
2025-04-23 20:54:15 +0530

    modified:   sentiment-analysis-backend/main.py

--- sentiment-analysis-backend/main.py
+++ sentiment-analysis-backend/main.py
@@ -10,7 +10,7 @@ import os
 from dotenv import load_dotenv
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins="*")
 load_dotenv()
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key_here')  # Use env var or fallback
 
